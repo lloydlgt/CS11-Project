05840f19576be02d2ab97e39bf6033b534179ce7
saltmemes <zapmoicunogaming@gmail.com>
2025-10-22 17:13:32 +0800

    fixed lloyds indentation

--- player.py
+++ player.py
@@ -1,8 +1,8 @@
 import tiles
-from mapper import map_converter 
+from mapper import stage 
 
 class player:
-    def __init__(self, location: tuple[int, int], bounds: tuple[int,int], curr_tile: str, map_1: map_converter, items: str, score: int):
+    def __init__(self, location: tuple[int, int], bounds: tuple[int,int], curr_tile: str, curr_stage: stage, items: str, score: int):
         self.x_coords = location[0]
         self.y_coords = location[1]
         self.coords = self.x_coords, self.y_coords
@@ -10,9 +10,8 @@ class player:
         self.y_bound = bounds[1]
         self.bounds = self.x_bound, self.y_bound
         self.curr_tile = curr_tile
-        self.map_1 = map_1
+        self.curr_stage = curr_stage
         self.items = items
-        self.score = score
     
     def move_up(self):
         """ #move up
@@ -49,8 +48,8 @@ class player:
     def move(self, loc: tuple[int,int]):
         if not loc:
             return False
-        if 0 <= loc[0] < len(self.map_1.maplist[0]) and 0 <= loc[1] < len(self.map_1.maplist):
-            tags = tiles.tile_tags[tiles.tiles_translate[self.map_1.maplist[loc[1]][loc[0]]]]
+        if 0 <= loc[0] < len(self.curr_stage.maplist[0]) and 0 <= loc[1] < len(self.curr_stage.maplist):
+            tags = tiles.tile_tags[tiles.tiles_translate[self.curr_stage.maplist[loc[1]][loc[0]]]]
             if "pushable" in tags:
                 #make dict of coords
                 #check if rock moved loc is in rock loc dict
@@ -58,14 +57,14 @@ class player:
                 # also account for player curr tile :) 
                 ...
 
-            if "can_move_to" in tags:
-                    self.map_1.maplist[self.prev_coords[1]][self.prev_coords[0]] = self.curr_tile # If you move away from current tile, this is the tile you are in before moving 
-                    self.curr_tile = self.map_1.maplist[loc[1]][loc[0]] # The tile you're currently on
-                    self.map_1.maplist[loc[1]][loc[0]] = "\N{adult}" # Sets the tile you moved to into the player (Adult)
-                    self.x_coords, self.y_coords = loc # The location of the current adult
+            if "can_move_to" in tags: # If you can move to the next tile
+                    self.curr_stage.maplist[self.prev_coords[1]][self.prev_coords[0]] = self.curr_tile #  Sets the tile you are currently on to what it was beforehand
+                    self.curr_tile = self.curr_stage.maplist[loc[1]][loc[0]] # Saves the tile you're going to
+                    self.curr_stage.maplist[loc[1]][loc[0]] = "\N{adult}" # Moves you to the next tile
+                    self.x_coords, self.y_coords = loc # Updates the player's coordinates
                     """if "manual_pickup" in tags and self.items in {"Empty"}:
                         mapstr = ""
-                        for row in self.map_1.maplist:
+                        for row in self.curr_stage.maplist:
                             mapstr += "".join(row) + "\n"
                         print(mapstr)
                         if pick_up_prompt in {'P', 'p'}:
@@ -73,21 +72,21 @@ class player:
                             self.curr_tile = """
                     if "win_condition" in tags:
                         self.curr_tile = "  "
-                        self.score += 1
+                        self.curr_stage.score += 1
                         
 
                     return False
             
             if "choppable" in tags or "burnable" in tags:
                     if self.items in {"x"}:
-                        self.map_1.maplist[self.prev_coords[1]][self.prev_coords[0]] = self.curr_tile
+                        self.curr_stage.maplist[self.prev_coords[1]][self.prev_coords[0]] = self.curr_tile
                         self.curr_tile = "  "
-                        self.map_1.maplist[loc[1]][loc[0]] = "\N{adult}"
+                        self.curr_stage.maplist[loc[1]][loc[0]] = "\N{adult}"
                         self.x_coords, self.y_coords = loc
                         self.items = "Empty"
                         return False
                     if self.items in {"*"}:
-                        self.map_1.maplist[self.prev_coords[1]][self.prev_coords[0]] = self.curr_tile
+                        self.curr_stage.maplist[self.prev_coords[1]][self.prev_coords[0]] = self.curr_tile
                         self.curr_tile = "  "
 
                         self.burn = {(loc[1], loc[0])}
@@ -95,15 +94,15 @@ class player:
                         while self.burn:
                             self.temp_burn = self.burn.copy()
                             for self.trees in self.burn:
-                                self.map_1.maplist[self.trees[0]][self.trees[1]] = "  "
+                                self.curr_stage.maplist[self.trees[0]][self.trees[1]] = "  "
                             self.burn.clear()
                             for self.trees in self.temp_burn:
                                 for self.direction in self.directions: # Bakit baliktad yung x, y dito wtf?!?
                                     if 0 <= self.trees[1] + self.direction[1] < self.x_bound and 0 <= self.trees[0] + self.direction[0] < self.y_bound:
-                                        if self.map_1.maplist[self.trees[0] + self.direction[0]][self.trees[1] + self.direction[1]] == tiles.translate_tiles['T']:
+                                        if self.curr_stage.maplist[self.trees[0] + self.direction[0]][self.trees[1] + self.direction[1]] == tiles.translate_tiles['T']:
                                             self.burn.add((self.trees[0] + self.direction[0], self.trees[1] + self.direction[1]))
 
-                        self.map_1.maplist[loc[1]][loc[0]] = "\N{adult}"
+                        self.curr_stage.maplist[loc[1]][loc[0]] = "\N{adult}"
                         self.x_coords, self.y_coords = loc
                         self.items = "Empty"
                         return False  
